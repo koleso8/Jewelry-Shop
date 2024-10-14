23528c7dc47e91da7cbcb71c4c5dce4a9fa630e9
koleso8 <dev.koleso@gmail.com>
2024-10-14 12:35:18 +0300

    fix

---
+++ src/helpers/api.js
@@ -0,0 +1,3 @@
+export const TG_BOT_TOKEN = '7781068895:AAEx82ihhnL7-l0SoE0XQS7oMNXe_W7Y4bY';
+export const TG_CHAT_ID = '@ukr_beads_b';
+export const API = `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`;
