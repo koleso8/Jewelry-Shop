d7c11b1c01cbeb4e3673af357d9ce4e127e567d1
koleso8 <dev.koleso@gmail.com>
2024-12-01 11:40:39 +0200

    Update AboutMe.jsx

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,7 +3,7 @@ import { scrollToTop } from '../helpers/scrollToTop';
 
 const AboutMe = () => {
   useEffect(() => scrollToTop());
-  return <div>About me</div>;
+  return <div>About me page</div>;
 };
 
 export default AboutMe;
